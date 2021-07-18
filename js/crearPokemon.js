58650a4aa9757c35179353b05f7a346b7e9740cb
DevArturoOrtueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2021-07-18 14:21:52 -0500

    add crearPokemon file

---
+++ js/crearPokemon.js
@@ -0,0 +1,37 @@
+
+export const crearPokemon = (pokemon) => {
+    const img = document.createElement('img');
+    img.src = pokemon.sprites.front_default;
+
+    const pname = document.createElement('h3');
+    pname.textContent = pokemon.name;
+
+    
+    const pstats = document.createElement('h4');
+    
+    pstats.textContent = 'Ability';
+
+    const pability = document.createElement('p');
+
+    const pdivstats = document.createElement('div');
+    pdivstats.setAttribute('style', 'flex');
+    pdivstats.appendChild(pstats);
+    pdivstats.appendChild(pability);
+
+    pokemon.abilities.forEach(element => {
+        const pability = document.createElement('label');
+        pability.setAttribute("class", "abilities");
+        pability.textContent = element.ability.name;
+        pdivstats.appendChild(pability);
+    });
+    
+    
+    const div = document.createElement('div');
+    div.appendChild(img);
+    div.appendChild(pname);
+    
+
+    div.appendChild(pdivstats);
+
+    pokemonContainer.appendChild(div);
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import {crearPokemon} from 'crearPokemon.js';
+
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 const pokemonContainer = document.querySelector(".pokemon-container");
@@ -19,37 +21,3 @@ const traerPokemon = (pokemon) => {
     .catch(error => swal("Error", "Pokemon no encontrado, revisa el nombre introducido...", "error"))
 }
 
-
-const crearPokemon = (pokemon) => {
-    const img = document.createElement('img');
-    img.src = pokemon.sprites.front_default;
-
-    const pname = document.createElement('h3');
-    pname.textContent = pokemon.name;
-
-    const pstats = document.createElement('h4');
-    pstats.textContent = 'Ability';
-
-    const pability = document.createElement('p');
-
-    const pdivstats = document.createElement('div');
-    pdivstats.setAttribute('style', 'flex');
-    pdivstats.appendChild(pstats);
-    pdivstats.appendChild(pability);
-    pokemon.abilities.forEach(element => {
-        const pability = document.createElement('label');
-        pability.setAttribute("class", "abilities");
-        pability.textContent = element.ability.name;
-        pdivstats.appendChild(pability);
-    });
-    
-    
-    const div = document.createElement('div');
-    div.appendChild(img);
-    div.appendChild(pname);
-    
-
-    div.appendChild(pdivstats);
-
-    pokemonContainer.appendChild(div);
-}
